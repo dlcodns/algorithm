6e1d4bc891b325e5d3db04c827667abed39fdb8a
chaeun lee <107669084+dlcodns@users.noreply.github.com>
2023-11-18 18:52:17 +0900

    주석달기

--- Sorting/Merged_Sorting/Kotlin.kt
+++ Sorting/Merged_Sorting/Kotlin.kt
@@ -1,10 +1,12 @@
 fun <T: Comparable<T>> List<T>.mergeSort(): List<T> {
     if (this.size <2) return this
+    //리스트의 반인덱스값
     val middle = this.size / 2
 
-    val left = this.subList(0,middle).mergeSort()
-    val right = this.subList(middle, this.size).mergeSort()
+    val left = this.subList(0,middle).mergeSort()//왼쪽 재귀 설정
+    val right = this.subList(middle, this.size).mergeSort() //오른쪽 재귀 설정
 
+    //머지 수행한 값을 main에 리턴
     return merge(left, right)
 }
 
@@ -14,26 +16,32 @@ private fun <T: Comparable<T>> merge(left: List<T>, right: List<T>): List<T> {
 
     val result = mutableListOf<T>()
 
+    //하나의 사이즈라도 넘어가면 끝
     while (leftIndex < left.size &&rightIndex < right.size){
         val leftElement = left[leftIndex]
         val rightElement = right[rightIndex]
         if(leftElement < rightElement) {
             result.add(leftElement)
             leftIndex += 1
+            //result에 더 작은 값 먼저 추가
         }
         else if(leftElement > rightElement) {
             result.add(rightElement)
             rightIndex += 1
+            //result에 더 작은 값 먼저 추가
         }
-        else{
+        else{//같으면 둘 다 추가
             result.add(leftElement)
             leftIndex += 1
             result.add(rightElement)
             rightIndex += 1
         }
     }
+    //왼쪽이 남으면
     if(leftIndex < left.size)
+        //나머지 값 result에 추가
         result.addAll(left.subList(leftIndex, left.size))
+    //반대로 진행
     if(rightIndex < right.size)
         result.addAll(right.subList(rightIndex, right.size))
 
