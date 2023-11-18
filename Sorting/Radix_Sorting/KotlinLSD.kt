d4ed10ef910b3341810aee70e1d807e35e7811f5
chaeun lee <107669084+dlcodns@users.noreply.github.com>
2023-11-18 18:20:04 +0900

    주석달기

--- Sorting/Radix_Sorting/KotlinLSD.kt
+++ Sorting/Radix_Sorting/KotlinLSD.kt
@@ -1,9 +1,12 @@
+//LSD : 작은 자릿값부터 정렬 시작
 fun MutableList<Int>.radixSortLSD() {
-    val base = 10
+    val base = 10   //십진수
     var done = false
-    var digits = 1
+    var digits = 1  //1의 자리부터 시작
+    //done이 true가 되면 stop
     while (!done) {
         done = true
+        //0~9까지 버킷 생성
         val buckets = arrayListOf<MutableList<Int>>().apply {
             for(i in 0..9) {
                 this.add(arrayListOf())
@@ -11,14 +14,15 @@ fun MutableList<Int>.radixSortLSD() {
         }
         this.forEach {
                 number ->
+            //remainingPart는 digits 자릿수 값까지만 포함한 남은 자릿수임
             val remainingPart = number / digits
-            val digit = remainingPart % base
-            buckets[digit].add(number)
+            val digit = remainingPart % base    //현재 자릿수의 값 구하기
+            buckets[digit].add(number)  //자릿수 값에 해당하는 버킷에 저장
             if(remainingPart > 0) {
                 done = false
             }
         }
-        digits *= base
+        digits *= base  //1, 10, 100, 1000
         this.clear()
         this.addAll(buckets.flatten())
     }
