f30884256da56fd26467190294e3df175b9722e9
chaeun lee <107669084+dlcodns@users.noreply.github.com>
2023-12-13 02:22:32 +0900

    Update insertiomSorting.kt

--- Sorting/Insertion_Sorting/insertiomSorting.kt
+++ Sorting/Insertion_Sorting/insertiomSorting.kt
@@ -5,13 +5,13 @@ fun <T> ArrayList<T>.swapAt(first: Int, second: Int){
 }
 
 fun <T: Comparable<T>> ArrayList<T>.insertionSort(showPasses: Boolean = false){
-    if(this.size < 2) return
+    if(this.size < 2) return  //숫자가 하나면 리턴
 
-    for (current in 1 until this.size){
-        for (shifting in (1 .. current).reversed()){
-            if(this[shifting] < this[shifting-1]){
+    for (current in 1 until this.size){                //1~size만큼 돌기
+        for (shifting in (1 .. current).reversed()){   //current~1만큼 돌기
+            if(this[shifting] < this[shifting-1]){     //오른쪽이 더 작으면 swap
                 this.swapAt(shifting, shifting-1)
-            } else{
+            } else{                                    //정렬되어 있으면 냅두기
                 break
             }
         }
