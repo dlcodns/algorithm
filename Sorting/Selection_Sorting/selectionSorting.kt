11a4a51c6eb7807ff55b2ee35fefdd7d67b302f1
chaeun lee <107669084+dlcodns@users.noreply.github.com>
2023-12-13 02:09:01 +0900

    Update selectionSorting.kt

--- Sorting/Selection_Sorting/selectionSorting.kt
+++ Sorting/Selection_Sorting/selectionSorting.kt
@@ -5,16 +5,16 @@ fun <T> ArrayList<T>.swapAt(first: Int, second: Int){
 }
 
 fun <T: Comparable<T>> ArrayList<T>.selectionSort(showPasses: Boolean = false){
-    if(this.size < 2) return
+    if(this.size < 2) return    //숫자가 하나면 리턴
 
-    for (current in 0 until (this.size-1)){
-        var lowest = current
-        for (other in (current+1) until this.size){
-            if(this[lowest] > this[other]){
+    for (current in 0 until (this.size-1)){    //0~size-1까지 돌기
+        var lowest = current                   //최솟값 비교 및 저장 위해 선언
+        for (other in (current+1) until this.size){    //current+1~size까지 돌기
+            if(this[lowest] > this[other]){            //더 작으면 대입
                 lowest = other
             }
         }
-        if(lowest != current){
+        if(lowest != current){            //그대로면 그냥 skip, 다르면 current와 swap
             this.swapAt(lowest,current)
         }
         if(showPasses) println(this)
