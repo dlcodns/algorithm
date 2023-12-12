e39e07811addd03fc8412173475d62e0763bc929
chaeun lee <107669084+dlcodns@users.noreply.github.com>
2023-12-13 01:21:55 +0900

    Update bubbleSorting.kt

--- Sorting/Bubble_Sorting/bubbleSorting.kt
+++ Sorting/Bubble_Sorting/bubbleSorting.kt
@@ -5,18 +5,18 @@ fun <T> ArrayList<T>.swapAt(first: Int, second: Int){
 }
 
 fun <T: Comparable<T>> ArrayList<T>.bubbleSort(showPasses: Boolean = false){
-    if(this.size < 2) return
+    if(this.size < 2) return    //숫자가 하나면 리턴
 
-    for (end in (1 until this.size).reversed()){
+    for (end in (1 until this.size).reversed()){    //뒤에서 1까지 돌기
         var swapped = false
-        for (current in 0 until end){
-            if(this[current] > this[current+1]){
-                this.swapAt(current,current+1)
+        for (current in 0 until end){    //0에서 end까지 돌기, 완료 시 end가 하나씩 줄어들거임
+            if(this[current] > this[current+1]){    //앞 원소가 더 크면
+                this.swapAt(current,current+1)      //swap
                 swapped = true
             }
         }
-        if(showPasses) println(this)
-        if(!swapped) return
+        if(showPasses) println(this)    //한 turn의 pass를 할 때 마다 print
+        if(!swapped) return             //바뀐 게 없는 건 이미 정렬돼있다는 뜻
     }
 }
 
