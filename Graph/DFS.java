e172f0ac011c699c49dd93384d6443f28bda747e
dlcodns <redmond0412@gmail.com>
2024-07-05 14:31:26 +0900

    no message

---
+++ Graph/DFS.java
@@ -0,0 +1,55 @@
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        Graph g = new Graph(7);
+
+        g.addEdge(0, 1);
+        g.addEdge(0, 3);
+        g.addEdge(1, 2);
+        g.addEdge(1, 3);
+        g.addEdge(1, 5);
+        g.addEdge(1, 6);
+        g.addEdge(2, 3);
+        g.addEdge(2, 4);
+        g.addEdge(2, 5);
+        g.addEdge(3, 4);
+        g.addEdge(4, 6);
+
+        System.out.println("DFS 0부터 시작(작은 숫자 우선 순위)");
+        g.DFS(0);
+    }
+}
+
+class Graph {
+    private int V;
+    private LinkedList<Integer> adj[];
+
+    Graph(int v) {
+        V = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; ++i)
+            adj[i] = new LinkedList<>();
+    }
+
+    public void addEdge(int v, int w) {
+        adj[v].add(w);
+        adj[w].add(v);
+    }
+
+    public void DFS(int s) {
+        boolean visited[] = new boolean[V];
+        DFSUtil(s, visited);
+    }
+
+    private void DFSUtil(int s, boolean visited[]) {
+        visited[s] = true;
+        System.out.print(s + " ");
+
+        for (int n : adj[s]) {
+            if (!visited[n]) {
+                DFSUtil(n, visited);
+            }
+        }
+    }
+}
---
+++ Search/loop_binary_search.java
@@ -0,0 +1,33 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = {2, 3, 4, 10, 40};
+        int target = 10;
+        int result = binarySearch(array, target);
+
+        if (result == -1) {
+            System.out.println("배열에 "+target+"이 없습니다.");
+        } else {
+            System.out.println(target+"은 index " + result+"에 있습니다.");
+        }
+    }
+
+    public static int binarySearch(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (array[mid] == target) {
+                return mid;
+            }
+            if (array[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
