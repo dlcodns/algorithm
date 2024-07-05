70f00a10d4c5fed93d8942baa467f7c4013a9622
chaeun lee <107669084+dlcodns@users.noreply.github.com>
2024-07-05 10:00:48 +0900

    Create mergeSort.java

---
+++ Sorting/Merged_Sorting/mergeSort.java
@@ -0,0 +1,89 @@
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.OutputStreamWriter;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        writer.write("정렬할 수들을 입력하세요. >>> ");
+        writer.flush();
+        String[] input = reader.readLine().split(" ");
+
+        int[] arr = new int[input.length];
+        for (int i = 0; i < input.length; i++) {
+            arr[i] = Integer.parseInt(input[i]);
+        }
+
+        writer.write("원래 배열: ");
+        printArray(writer, arr);
+
+        mergeSort(arr, 0, arr.length - 1);
+
+        writer.write("병합 정렬 후 배열: ");
+        printArray(writer, arr);
+    }
+
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+
+            merge(arr, left, mid, right);
+        }
+    }
+
+    public static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            L[i] = arr[left + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            R[j] = arr[mid + 1 + j];
+        }
+
+        int i = 0, j = 0;
+        int k = left;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void printArray(BufferedWriter writer, int[] arr) throws IOException {
+        for (int j : arr) {
+            writer.write(j + " ");
+        }
+        writer.write("\n\n");
+        writer.flush();
+    }
+}
